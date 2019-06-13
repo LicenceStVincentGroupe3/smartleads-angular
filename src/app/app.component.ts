b494541cd54cfd247a502ac2ff7bce66ab1a3080
DylanNoally <dylan.window@hotmail.fr>
2019-06-13 11:12:59 +0200

    Correctifs dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  // title = 'smartleads-angular';
-  date = Date.now();
+export class AppComponent implements OnInit {
+    // title = 'smartleads-angular';
+    date: any;
+    constructor() {
+    }
+
+    ngOnInit() {
+        const arrayDays = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+        const arrayMonth = new Array(
+            'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+        let datetime = new Date();
+
+        let year = datetime.getFullYear();
+        let month = arrayMonth[datetime.getMonth()];
+        let dayNumber = datetime.getDate();
+        let dayLetter = arrayDays[datetime.getDay()];
+
+        this.date = dayLetter + ' ' + dayNumber.toString() + ' ' + month + ' ' + year.toString();
+    }
 }
